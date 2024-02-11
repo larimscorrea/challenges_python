dffc0749025e71ef76a51e742e7c658feb1ac734
Larissa Corrêa <larimscorrea@gmail.com>
2024-02-10 23:23:52 -0300

    third challenges

---
+++ 2.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+
+t = Turtle()
+
+t.speed(1)
+
+t.forward(100)
+
+t.right(90)
+t.forward(100)
+
+t.left(90)
+t.forward(100)
+
+t.backward(200)
+
+while True:
+    distance = int(input('Distancia a percorrer?'))
+    t.forward(distance)
